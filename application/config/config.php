00a46b2913ab973731ce034d77e0e8ea6d759561
Steven Fu <sfu@synvata.com>
2016-10-10 16:42:55 +0800

    fixed server base_url issue.

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://g.thoughts-go.top/warehouse/index.php?';
+$config['base_url'] = 'http://g.thoughts-go.top/warehouse';
 
 /*
 |--------------------------------------------------------------------------
